ead98b597fa9c00dfa2b50aabe29ce3ef0d83949
Brad <brad@brads-mbp.lan>
2024-02-20 20:42:30 -0800

    rename nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const inter = Inter({
   display: "swap",
 });
 
-function Intro() {
+function Nav() {
   return (
     <section  className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12 mx-1.5" >
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
@@ -75,7 +75,7 @@ export default function RootLayout({
     <html lang="en" className={`${inter.className} antialiased`}>
       <body>
         <section className="min-h-screen">
-        <Intro />
+        <Nav />
           <main>{children}</main>
           <Footer />
         </section>
