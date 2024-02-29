44f5fb70a64b4ddfd595609ceefd2fcc4bc423e3
Brad <brad@brads-mbp.lan>
2024-02-29 12:00:11 -0800

    update image config

--- next.config.js
+++ next.config.js
@@ -4,4 +4,11 @@ module.exports = {
     loader: "custom",
     formats: ["image/avif", "image/webp"],
   },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'images.ctfassets.net',
+      port: ''
+    }
+  ]
 };
