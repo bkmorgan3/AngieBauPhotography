6a9280fbf30d49d083c75df5560692ff9684b4fe
Brad <brad@brads-mbp.lan>
2024-03-03 23:47:21 -0800

    create mobile nav list

--- app/components/DesktopNav.tsx
+++ app/components/DesktopNav.tsx
@@ -3,28 +3,28 @@ import Link from  "next/link";
 
 export default function DesktopNav() {
     return (
-         <div className="hidden md:flex md:text-left text-lg mt-5 md:pl-8">
-       <Link href={`/`} className="mx-1.5 hover:underline">
-              Highlights
-        </Link>
-       <Link href={`/commercial/`} className="mx-1.5 hover:underline">
-              Commercial
-        </Link>
-       <Link href={`/people/`} className="mx-1.5 hover:underline">
-              People
-        </Link>
-       <Link href={`/motion/`} className="mx-1.5 hover:underline">
-              Motion
-        </Link>
-       <Link href={`/installations/`} className="mx-1.5 hover:underline">
-              Installations
-        </Link>
-       <Link href={`/about/`} className="mx-1.5 hover:underline">
-              About
-        </Link>
-       <Link href={`/contact/`} className="mx-1.5 hover:underline">
-              Contact
-        </Link>
+      <div className="hidden md:flex md:text-left text-lg mt-5 md:pl-8">
+            <Link href={`/`} className="mx-1.5 hover:underline">
+                  Highlights
+            </Link>
+            <Link href={`/commercial/`} className="mx-1.5 hover:underline">
+                  Commercial
+            </Link>
+            <Link href={`/people/`} className="mx-1.5 hover:underline">
+                  People
+            </Link>
+            <Link href={`/motion/`} className="mx-1.5 hover:underline">
+                  Motion
+            </Link>
+            <Link href={`/installations/`} className="mx-1.5 hover:underline">
+                  Installations
+            </Link>
+            <Link href={`/about/`} className="mx-1.5 hover:underline">
+                  About
+            </Link>
+            <Link href={`/contact/`} className="mx-1.5 hover:underline">
+                  Contact
+            </Link>
       </div>
     )
 }
--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -1,13 +1,40 @@
 'use client';
 import Link from  "next/link";
 
+type MobileProps = {
+    setIsOpen: () => void
+    isOpen: boolean
+}
 
 
-export default function MobileNav({setIsOpen, isOpen}) {
+export default function MobileNav({setIsOpen, isOpen}: MobileProps) {
       
     return (
-        <div>
-            <button onClick={() => setIsOpen(!isOpen)}>X</button>
+        <div className="flex flex-col">
+            <button className="self-end" onClick={() => setIsOpen(!isOpen)}>X</button>
+             <div style={{border: '1px solid red'}} className="flex flex-col  text-lg mt-5 pl-8">
+                <Link onClick={() => setIsOpen(!isOpen)} href={`/`} className="mx-1.5 hover:underline">
+                        Highlights
+                    </Link>
+                <Link onClick={() => setIsOpen(!isOpen)} href={`/commercial/`} className="mx-1.5 hover:underline">
+                        Commercial
+                    </Link>
+                <Link onClick={() => setIsOpen(!isOpen)} href={`/people/`} className="mx-1.5 hover:underline">
+                        People
+                    </Link>
+                <Link href={`/motion/`} className="mx-1.5 hover:underline">
+                        Motion
+                    </Link>
+                <Link onClick={() => setIsOpen(!isOpen)} href={`/installations/`} className="mx-1.5 hover:underline">
+                        Installations
+                    </Link>
+                <Link onClick={() => setIsOpen(!isOpen)} href={`/about/`} className="mx-1.5 hover:underline">
+                        About
+                    </Link>
+                <Link onClick={() => setIsOpen(!isOpen)} href={`/contact/`} className="mx-1.5 hover:underline">
+                        Contact
+                    </Link>
+            </div>
         </div>
     )
 }
