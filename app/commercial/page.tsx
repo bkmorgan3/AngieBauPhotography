043c7a8c8062e4781a9b0c54727115015ee8b230
Brad <brad@brads-mbp.lan>
2024-03-04 12:46:51 -0800

    WIP: image layout

--- app/commercial/page.tsx
+++ app/commercial/page.tsx
@@ -7,22 +7,20 @@ function cn(...classes: any[]) {
 }
 
 export default async function CommercialPage() {
-      const { isEnabled } = draftMode();
+  const { isEnabled } = draftMode();
 
   const allPhotos = await getCommercialPhotos(isEnabled)
 
   return (
-    <div className="container flex flex-wrap justify-center mx-auto px-5">
+    <div className="flex flex-wrap justify-center items-start mx-auto px-5">
       {allPhotos.map(photo => (
         <ContentfulImage
           key={photo.photo.title}
           alt={photo.photo.description}
-          width={700}
-          height={600}
-          className={cn("md:w-1/2 shadow-small", {
-        "hover:shadow-medium transition-shadow duration-200": photo.photo.url,
-      })}
-        src={photo.photo.url}
+          width={1000}
+          height={700}
+          src={photo.photo.url}
+          className="object-fit"
         />
       ))}
   </div>
