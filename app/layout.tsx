a989653b5a99b7f3be5a8c5929bbe91a8968b94b
Brad <brad@brads-mbp.lan>
2024-03-03 21:43:09 -0800

    hamburger icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,14 @@ function Nav() {
               Contact
         </Link>
       </div>
-        <div className="flex md:hidden">Burger</div>
+        <div className="flex md:hidden">
+         <button className="navbar-burger flex items-center text-blue-600 p-3">
+          <svg className="block h-4 w-4 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+					<title>Mobile menu</title>
+					<path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+				</svg>
+			</button>
+        </div>
       
     </section>
   );
