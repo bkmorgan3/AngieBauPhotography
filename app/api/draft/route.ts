012abe8aedb8f88aee4c7d10c1680e51abe306d6
Brad <brad@brads-mbp.lan>
2024-03-11 14:52:31 -0700

    remove unused code

--- app/api/draft/route.ts
+++ app/api/draft/route.ts
@@ -1,6 +1,4 @@
 import { draftMode } from "next/headers";
-import { redirect } from "next/navigation";
-import { getPreviewPostBySlug } from "../../../lib/api";
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
@@ -11,12 +9,6 @@ export async function GET(request: Request) {
     return new Response("Invalid token", { status: 401 });
   }
 
-  const post = await getPreviewPostBySlug(slug);
-
-  if (!post) {
-    return new Response("Invalid slug", { status: 401 });
-  }
-
   draftMode().enable();
-  redirect(`/posts/${post.slug}`);
+ 
 }
