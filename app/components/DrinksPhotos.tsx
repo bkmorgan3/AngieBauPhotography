14751b780e710359028973b12987036adfbe869e
Brad <brad@brads-mbp.lan>
2024-03-25 14:20:43 -0700

    initial layout order

--- app/components/DrinksPhotos.tsx
+++ app/components/DrinksPhotos.tsx
@@ -6,18 +6,37 @@ export default async function DrinksPhotos() {
      const { isEnabled } = draftMode();
 
   const allPhotos = await getPhotosByTag(isEnabled, "drinks")
-  
+  console.log('a',allPhotos)
+  const drinker = allPhotos[0]
+  const skater = allPhotos[1]
+  const pineapple = allPhotos[2]
+  const can = allPhotos[3]
   return (
-    <div className="container gap-1 flex flex-wrap justify-center items-start mx-auto px-5 overflow-hidden">
-        {allPhotos.map(photo => (
+    <div className="container mx-auto px-5 mt-1 ">
+      <div className="gallery hidden lg:grid">
+        <div className="can">
+          <ContentfulImage src={can.photo.url} alt={can.photo.description} width={500} height={10} />
+        </div>
+        <div  className="skater">
+          <ContentfulImage src={skater.photo.url} alt={skater.photo.description} width={500} height={10} />
+        </div>
+        <div className="drinker">
+          <ContentfulImage src={drinker.photo.url} alt={drinker.photo.description} width={500} height={10}/>
+        </div>
+        <div className="pineapple">
+          <ContentfulImage src={pineapple.photo.url} alt={pineapple.photo.description}width={600} height={10} />
+        </div>
+      </div>
+      <div className="flex flex-wrap justify-center lg:hidden mx-auto gap-1">
+        {allPhotos.map(img => (
           <ContentfulImage
-            src={photo.photo.url}
-            alt={photo.photo.description}
+            src={img.photo.url}
+            alt={img.photo.description}
             width={500}
-            height={200}
-            className="w-auto"
-          />
+            height={100}
+            />
         ))}
+      </div>
     </div>
   )
 }
