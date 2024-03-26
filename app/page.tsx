35b661867745f10d77a782b6a56110faf01913c8
Brad <brad@brads-mbp.lan>
2024-03-25 22:56:52 -0700

    reflow highlights

--- app/page.tsx
+++ app/page.tsx
@@ -2,29 +2,49 @@ import { draftMode } from "next/headers";
 import {  getPhotosByTag } from "@/lib/api";
 import ContentfulImage from "@/lib/contentful-image";
 
-function cn(...classes: any[]) {
-  return classes.filter(Boolean).join(" ");
-}
-
 export default async function Page() {
   const { isEnabled } = draftMode();
 
   const allPhotos = await getPhotosByTag(isEnabled, "highlights")
 
+  const venice = allPhotos[0]
+  const makeup = allPhotos[1]
+  const lemonade = allPhotos[2]
+  const pablo = allPhotos[3]
+  const honey = allPhotos[4]
+
   return (
-    <div  className="container flex flex-wrap  justify-center gap-1 items-start mx-auto px-5">
-      {allPhotos.map(photo => (
-        <ContentfulImage
-          key={photo.photo.title}
-          alt={photo.photo.description}
-          width={500}
-          height={600}
-          className={cn("shadow-small", {
-          "hover:shadow-medium transition-shadow duration-200": photo.photo.url,
-          })}
-          src={photo.photo.url}
-        />
-      ))}
+    <div  className="container flex flex-wrap justify-center gap-1 items-start mx-auto px-5">
+      <ContentfulImage 
+        src={venice.photo.url}
+        alt={venice.photo.description}
+        width={500}
+        height={100}
+      />
+      <ContentfulImage 
+        src={lemonade.photo.url}
+        alt={lemonade.photo.description}
+        width={500}
+        height={100}
+      />
+      <ContentfulImage 
+        src={makeup.photo.url}
+        alt={makeup.photo.description}
+        width={500}
+        height={100}
+      />
+      <ContentfulImage 
+        src={pablo.photo.url}
+        alt={pablo.photo.description}
+        width={500}
+        height={100}
+      />
+      <ContentfulImage 
+        src={honey.photo.url}
+        alt={honey.photo.description}
+        width={500}
+        height={100}
+      />
     </div>
   );
 }
