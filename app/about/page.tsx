4a5e3f94f1dcb4b86220414158f1c5a5b880a02b
Brad <brad@brads-mbp.lan>
2024-04-01 12:36:12 -0700

    edit layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,20 +11,18 @@ export default async function AboutPage() {
   
     return (
         <div className="container flex flex-col sm:flex-row flex-wrap mx-auto ">
-                    <ContentfulImage 
-                        src={angie.photo.url}
-                        alt={angie.photo.description}
-                        width={500}
-                        height={200}
-                        key={angie.photo.url}
-                        className="rounded-md sm:w-1/2 mx-auto"
-                    />
-               
-                
-                    <p className="sm:w-1/2 self-center px-3 mt-2">Angie lives in Los Angeles, CA.  Her award winning photos have been featured in publications and galleries far and wide.  
-                    When she is not behind the camera you can find her in a dance class or out hiking with her dog Pablo.  
-                    If you would like to hire Angie or talk about any future projects you can send a message in the email below or on the Contact page. 
-                    Thanks for stopping by the site.</p>
+            <ContentfulImage 
+                src={angie.photo.url}
+                alt={angie.photo.description}
+                width={500}
+                height={200}
+                key={angie.photo.url}
+                className="rounded-md sm:w-1/2 mx-auto sm:mb-5"
+            />
+            <p className="sm:w-1/2 self-center p-4 mt-3 mb-3 sm:text-justify mx-auto">Angie lives in Los Angeles, CA.  Her award winning photos have been featured in publications and galleries far and wide.  
+            When she is not behind the camera you can find her in a dance class or out hiking with her dog Pablo.  
+            If you would like to hire Angie or talk about any future projects you can send a message in the email below or on the Contact page. 
+            Thanks for stopping by the site.</p>
         </div>
     )
 }
