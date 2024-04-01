a5e5626efae7d1eead28ef713a9a82cbe8632eb8
Brad <brad@brads-mbp.lan>
2024-04-01 12:22:19 -0700

    adjust layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,27 +6,25 @@ export default async function AboutPage() {
     const { isEnabled } = draftMode();
 
     const allPhotos = await getPhotosByTag(isEnabled, "about")
+      const [angie] = allPhotos
+
   
     return (
-        <div  className="container flex flex-wrap mx-auto justify-center h-full">
-            <div className="image">
-            {allPhotos.map(img => (
-                <ContentfulImage 
-                    src={img.photo.url}
-                    alt={img.photo}
-                    width={500}
-                    height={10}
-                    key={img.photo.url}
-                    className="rounded-md"
+        <div className="container flex flex-col sm:flex-row flex-wrap mx-auto ">
+                    <ContentfulImage 
+                        src={angie.photo.url}
+                        alt={angie.photo.description}
+                        width={500}
+                        height={200}
+                        key={angie.photo.url}
+                        className="rounded-md sm:w-1/2 mx-auto"
                     />
-            ))}
-            </div>
-                <div className="text w-1/2 self-center mx-3 sm:my-1">
-                    Angie lives in Los Angeles, CA.  Her award winning photos have been featured in publications and galleries far and wide.  
+               
+                
+                    <p className="sm:w-1/2 self-center px-3 mt-2">Angie lives in Los Angeles, CA.  Her award winning photos have been featured in publications and galleries far and wide.  
                     When she is not behind the camera you can find her in a dance class or out hiking with her dog Pablo.  
                     If you would like to hire Angie or talk about any future projects you can send a message in the email below or on the Contact page. 
-                    Thanks for stopping by the site.
-                </div>
+                    Thanks for stopping by the site.</p>
         </div>
     )
 }
